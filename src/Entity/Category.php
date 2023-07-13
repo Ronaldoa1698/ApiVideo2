7ebdd35049df42bc9e7994ed7353791980fbb823
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2023-07-12 23:43:12 -0500

    relationship category-video, user-category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -25,6 +27,18 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $cover = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Video::class, orphanRemoval: true)]
+    private Collection $videos;
+
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'categories')]
+    private Collection $UserCategory;
+
+    public function __construct()
+    {
+        $this->videos = new ArrayCollection();
+        $this->UserCategory = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -75,6 +89,63 @@ class Category
     public function setCover(string $cover): static
     {
         $this->cover = $cover;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Video>
+     */
+    public function getVideos(): Collection
+    {
+        return $this->videos;
+    }
+
+    public function addVideo(Video $video): static
+    {
+        if (!$this->videos->contains($video)) {
+            $this->videos->add($video);
+            $video->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVideo(Video $video): static
+    {
+        if ($this->videos->removeElement($video)) {
+            // set the owning side to null (unless already changed)
+            if ($video->getCategory() === $this) {
+                $video->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUserCategory(): Collection
+    {
+        return $this->UserCategory;
+    }
+
+    public function addUserCategory(User $userCategory): static
+    {
+        if (!$this->UserCategory->contains($userCategory)) {
+            $this->UserCategory->add($userCategory);
+            $userCategory->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserCategory(User $userCategory): static
+    {
+        if ($this->UserCategory->removeElement($userCategory)) {
+            $userCategory->removeCategory($this);
+        }
 
         return $this;
     }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -30,6 +32,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $photo = null;
+
+    #[ORM\ManyToMany(targetEntity: Category::class, inversedBy: 'UserCategory')]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -109,6 +119,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPhoto(?string $photo): static
     {
         $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Category>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): static
+    {
+        $this->categories->removeElement($category);
 
         return $this;
     }
